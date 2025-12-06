f52fe7e6ab58d51c4333f87c0ddf33c03c126370
pankaj0417 <pankaj0172004@gmail.com>
2025-12-07 05:12:04 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ connectDB();
 
 app.use(
   cors({
-    origin:"http://localhost:5173",
+    origin:["http://localhost:5173", "https://assign-hubcredo.onrender.com"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
